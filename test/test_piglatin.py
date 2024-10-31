a7668fb7cf509e50b4af85eb68d789690ac1a078
lottarantala <lottarantala20@gmail.com>
2024-10-31 16:58:09 +0200

    US2 - test 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -8,3 +8,7 @@ class TestPigLatin(unittest.TestCase):
     def test_constructor(self):
         translator = PigLatin("This is a phrase")
         self.assertEqual("This is a phrase", translator.get_phrase())
+
+    def test_translate_empty_string(self):
+        translator = PigLatin("")
+        self.assertEqual("nil", translator.translate())
