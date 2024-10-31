ae5396c99b2eed685331bbbb8f3ab5ee3e17655a
lottarantala <lottarantala20@gmail.com>
2024-10-31 17:19:40 +0200

    US6 - test 2

--- piglatin.py
+++ piglatin.py
@@ -16,18 +16,27 @@ class PigLatin:
         vowels = 'aeiou'
         
         for word in words:
-            if word[0] in vowels:
-                if word[-1] == 'y':
-                    translated_word = word + 'nay'
-                else:
-                    translated_word = word + 'ay'
+            if '-' in word:
+                subwords = word.split('-')
+                translated_subwords = [self.translate_subword(subword, vowels) for subword in subwords]
+                translated_word = '-'.join(translated_subwords)
             else:
-                first_vowel_idx = len(word)
-                for i, char in enumerate(word):
-                    if char in vowels:
-                        first_vowel_idx = i
-                        break
-                translated_word = word[first_vowel_idx:] + word[:first_vowel_idx] + 'ay'
+                translated_word = self.translate_subword(word, vowels)
             translated_words.append(translated_word)
         
         return ' '.join(translated_words)
+
+    def translate_subword(self, word, vowels):
+        if word[0] in vowels:
+            if word[-1] == 'y':
+                return word + 'nay'
+            else:
+                return word + 'ay'
+        else:
+            first_vowel_idx = len(word)
+            for i, char in enumerate(word):
+                if char in vowels:
+                    first_vowel_idx = i
+                    break
+            return word[first_vowel_idx:] + word[:first_vowel_idx] + 'ay'
+
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -36,3 +36,7 @@ class TestPigLatin(unittest.TestCase):
     def test_translate_many_words(self):
         translator = PigLatin("hello world")
         self.assertEqual("ellohay orldway", translator.translate())
+
+    def test_translate_composite_word(self):
+        translator = PigLatin("well-being")
+        self.assertEqual("ellway-eingbay", translator.translate())
